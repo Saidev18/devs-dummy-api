32b78b5919681454d8c90bbd618e68628b386726
Saidev18 <chakkasaidev18@email.com>
2024-02-28 16:12:02 +0530

    first commit

---
+++ posts.js
@@ -0,0 +1,58 @@
+module.exports=[
+    {
+      "_id": "65de28bc7d6c7b77ff9c4c4e",
+      "isActive": true,
+      "picture": "http://placehold.it/32x32",
+      "name": "Kemp Wallace",
+      "gender": "male",
+      "email": "kempwallace@rockyard.com"
+    },
+    {
+      "_id": "65de28bcf5a59b9c89d0586c",
+      "isActive": true,
+      "picture": "http://placehold.it/32x32",
+      "name": "Marquita Cunningham",
+      "gender": "female",
+      "email": "marquitacunningham@rockyard.com"
+    },
+    {
+      "_id": "65de28bc57b511ae5f10d1c4",
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Wheeler Dejesus",
+      "gender": "male",
+      "email": "wheelerdejesus@rockyard.com"
+    },
+    {
+      "_id": "65de28bcf9d75e7b13339cdb",
+      "isActive": true,
+      "picture": "http://placehold.it/32x32",
+      "name": "Ochoa Norman",
+      "gender": "male",
+      "email": "ochoanorman@rockyard.com"
+    },
+    {
+      "_id": "65de28bc801cd3b111f6e0ec",
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Mildred Ashley",
+      "gender": "female",
+      "email": "mildredashley@rockyard.com"
+    },
+    {
+      "_id": "65de28bcc4487f079ecc311e",
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Alejandra Buckley",
+      "gender": "female",
+      "email": "alejandrabuckley@rockyard.com"
+    },
+    {
+      "_id": "65de28bc940f6d60221f6f0a",
+      "isActive": false,
+      "picture": "http://placehold.it/32x32",
+      "name": "Hensley Todd",
+      "gender": "male",
+      "email": "hensleytodd@rockyard.com"
+    }
+  ]
